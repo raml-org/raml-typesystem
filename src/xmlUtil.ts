bec3b9671251648ef959e4a35edee40777cfd9f6
dreamflyer <gleb@onpositive.com>
2016-04-23 04:52:58 +0600

    modules manage

--- src/xmlUtil.ts
+++ src/xmlUtil.ts
@@ -1,16 +1,25 @@
 /// <reference path="../typings/main.d.ts" />
 declare function require(s:string):any;
 
-var xmllint = require('libxml-xsd');
+var xmllint1 = require('libxml-xsd');
+var xmllint2 = require('xmllint');
 
 export class XMLValidator {
     private schemaObject: any;
     
     constructor(private schema:string) {
-        this.schemaObject = xmllint.parse(schema);
+        this.schemaObject = xmllint1.parse(schema);
     }
 
     validate(xml: string): Error[] {
+        if(isAtom()) {
+            return [];
+        }
+        
+        if(isBrowser()) {
+            return xmllint2.validateXML({xml: xml, schema: this.schema});
+        }
+        
         return this.schemaObject.validate(xml);
     }
 }
@@ -77,6 +86,18 @@ function objectToXml(object: any) {
     result = result + '</' + nodeName + '>';
 
     return result;
+}
+
+function isBrowser() {
+    typeof window !== "undefined" && window && !(<any>window).atom;
+}
+
+function isAtom() {
+    typeof window !== "undefined" && window && (<any>window).atom;
+}
+
+function isNode() {
+    typeof window === "undefined";
 }
 
 export function jsonToXml(jsonObject: any) {
