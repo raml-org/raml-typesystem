33964ea0a0d65fb75fc8b015f17f1c4b78ac893d
dreamflyer <gleb@onpositive.com>
2016-04-23 07:05:01 +0600

    minor fix

--- src/xmlUtil.ts
+++ src/xmlUtil.ts
@@ -1,13 +1,16 @@
 /// <reference path="../typings/main.d.ts" />
 declare function require(s:string):any;
 
+var xmllint1 = require('libxml-xsd');
 var xmllint2 = require('xmllint');
 
 export class XMLValidator {
     private schemaObject: any;
 
     constructor(private schema:string) {
-        
+        if(isNode()) {
+            this.schemaObject = xmllint1.parse(schema);
+        }
     }
 
     validate(xml: string): Error[] {
@@ -19,6 +22,10 @@ export class XMLValidator {
             var result = xmllint2.validateXML({xml: xml, schema: this.schema});
 
             return (result && result.errors) || [];
+        }
+
+        if(isNode()) {
+            return this.schemaObject.validate(xml);
         }
 
         return [];
