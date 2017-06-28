2027e43b6c7f0c9269654a37da6bfe39937512ee
KonstantinSviridov <ksviridov@gmail.com>
2017-06-28 23:40:36 +0600

    fix https://www.mulesoft.org/jira/browse/RP-58

--- src/typesystem.ts
+++ src/typesystem.ts
@@ -1785,6 +1785,12 @@ export class InheritedType extends AbstractType{
                 (<any>this)[prop] = (<any>another)[prop];
             }
         }
+        for(let i = 0 ; i < this._superTypes.length; i++){
+            var st = this._superTypes[i];
+            if(st == this || st.allSuperTypes().indexOf(this)>=0){
+                this._superTypes[i] = RECURRENT;
+            }
+        }
     }
 
 }
