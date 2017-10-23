37c0f1c0993fbbb638de73ecdb2999036d52c135
KonstantinSviridov <ksviridov@gmail.com>
2017-10-23 17:03:57 +0600

    fix https://www.mulesoft.org/jira/browse/RP-177

--- src/typesystem.ts
+++ src/typesystem.ts
@@ -2455,7 +2455,11 @@ export class IntersectionType extends DerivedType{
 
     restrictions(){
         var rs:Constraint[]=[];
-        this.allOptions().forEach(x=>rs=rs.concat(x.restrictions()));
+        this.allOptions().forEach(x=>{
+            if(!x.oneMeta(metaInfo.SkipValidation)) {
+                rs = rs.concat(x.restrictions());
+            }
+        });
         return [new AndRestriction(rs)]
     }
 
